f774e3064eca8892e1cc36b66527cb0c325a9536
m0cchi <boom.boom.planet@gmail.com>
2017-10-29 17:40:51 +0900

    add Delete Service API

--- model/service.go
+++ model/service.go
@@ -1,12 +1,16 @@
 package model
 
 import (
+	"errors"
 	"github.com/go-sql-driver/mysql"
 	"github.com/m0cchi/gfalcon"
 )
 
 const SQL_GET_SERVICE_BY_ID = "SELECT `iid`, `id` FROM `services` WHERE `id` = :service_id"
 const SQL_CREATE_SERVICE = "INSERT INTO `services` (`id`) VALUE (:service_id)"
+
+const SQL_DELETE_SERVICE_BY_IID = "DELETE FROM `services` WHERE `iid` = :service_iid"
+const SQL_DELETE_SERVICE_BY_ID = "DELETE FROM `services` WHERE `id` = :service_id"
 
 type Service struct {
 	IID uint32 `db:"iid"`
@@ -55,4 +59,42 @@ func CreateService(db gfsql.DB, serviceID string) (*Service, error) {
 	}
 
 	return service, err
+}
+
+func DeleteServiceByIID(db gfsql.DB, serviceIID uint32) error {
+	stmt, err := db.PrepareNamed(SQL_DELETE_SERVICE_BY_IID)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	args := map[string]interface{}{"service_iid": serviceIID}
+	_, err = stmt.Exec(args)
+	return err
+}
+
+func DeleteServiceByID(db gfsql.DB, serviceID string) error {
+	stmt, err := db.PrepareNamed(SQL_DELETE_SERVICE_BY_ID)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	args := map[string]interface{}{"service_id": serviceID}
+	_, err = stmt.Exec(args)
+	return err
+}
+
+func (service *Service) Delete(db gfsql.DB) error {
+	if service == nil {
+		return errors.New("not specify service")
+	}
+
+	if service.IID != 0 {
+		return DeleteServiceByIID(db, service.IID)
+	} else if service.ID != "" {
+		return DeleteServiceByID(db, service.ID)
+	}
+
+	return errors.New("not specify service")
 }
