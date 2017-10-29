cbb497286b1cbead55a84d486b7dd7cc16f87f42
m0cchi <boom.boom.planet@gmail.com>
2017-10-29 23:51:57 +0900

    remove auto-reset for table

--- metatools/sql/create_defaultdata.sql
+++ metatools/sql/create_defaultdata.sql
@@ -1,7 +1,4 @@
-SOURCE create_table.sql;
-
 INSERT INTO `services`(`id`) VALUE ('gfalcon');
 INSERT INTO `actions`(`service_iid`,`id`) SELECT `iid` as `service_iid`, 'all' as `id` FROM `services` WHERE `services`.`id` = 'gfalcon';
 INSERT INTO `teams`(`id`) VALUE ('gfalcon');
 INSERT INTO `users`(`team_iid`,`id`) SELECT `iid` as `team_iid`, 'gfadmin' as `id` FROM `teams` WHERE `teams`.`id` = 'gfalcon';
-
