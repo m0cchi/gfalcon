2b55afca09298ca88681a08fe8f632e0ce354bc0
m0cchi <boom.boom.planet@gmail.com>
2017-10-29 22:31:49 +0900

    add Delete Team API

--- model/team.go
+++ model/team.go
@@ -1,12 +1,16 @@
 package model
 
 import (
+	"errors"
 	"github.com/go-sql-driver/mysql"
 	"github.com/m0cchi/gfalcon"
 )
 
 const SQL_GET_TEAM_BY_ID = "SELECT iid, id FROM `teams` WHERE `id` = :team_id"
 const SQL_CREATE_TEAM = "INSERT INTO `teams` (`id`) VALUE (:team_id)"
+
+const SQL_DELETE_TEAM_BY_IID = "DELETE FROM `teams` WHERE `iid` = :team_iid"
+const SQL_DELETE_TEAM_BY_ID = "DELETE FROM `teams` WHERE `id` = :team_id"
 
 type Team struct {
 	IID uint32 `db:"iid"`
@@ -54,4 +58,42 @@ func CreateTeam(db gfsql.DB, teamID string) (*Team, error) {
 	}
 
 	return team, err
+}
+
+func DeleteTeamByIID(db gfsql.DB, teamIID uint32) error {
+	stmt, err := db.PrepareNamed(SQL_DELETE_TEAM_BY_IID)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	args := map[string]interface{}{"team_iid": teamIID}
+	_, err = stmt.Exec(args)
+	return err
+}
+
+func DeleteTeamByID(db gfsql.DB, teamID string) error {
+	stmt, err := db.PrepareNamed(SQL_DELETE_TEAM_BY_ID)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	args := map[string]interface{}{"team_id": teamID}
+	_, err = stmt.Exec(args)
+	return err
+}
+
+func (team *Team) Delete(db gfsql.DB) error {
+	if team == nil {
+		return errors.New("not specify team")
+	}
+
+	if team.IID != 0 {
+		return DeleteTeamByIID(db, team.IID)
+	} else if team.ID != "" {
+		return DeleteTeamByID(db, team.ID)
+	}
+
+	return errors.New("not specify team")
 }
