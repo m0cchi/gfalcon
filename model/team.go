51ae51fa9133dc7ec19fb18ab4ed868ec4bea389
m0cchi <boom.boom.planet@gmail.com>
2017-10-29 15:17:52 +0900

    add Register Team API

--- model/team.go
+++ model/team.go
@@ -1,10 +1,12 @@
 package model
 
 import (
+	"github.com/go-sql-driver/mysql"
 	"github.com/m0cchi/gfalcon"
 )
 
 const SQL_GET_TEAM_BY_ID = "SELECT iid, id FROM `teams` WHERE `id` = :team_id"
+const SQL_CREATE_TEAM = "INSERT INTO `teams` (`id`) VALUE (:team_id)"
 
 type Team struct {
 	IID uint32 `db:"iid"`
@@ -21,5 +23,35 @@ func GetTeam(db gfsql.DB, teamID string) (*Team, error) {
 	team := &Team{}
 	args := map[string]interface{}{"team_id": teamID}
 	err = stmt.Get(team, args)
+	return team, err
+}
+
+func CreateTeam(db gfsql.DB, teamID string) (*Team, error) {
+	stmt, err := db.PrepareNamed(SQL_CREATE_TEAM)
+
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	args := map[string]interface{}{"team_id": teamID}
+	result, err := stmt.Exec(args)
+	team := &Team{
+		IID: 0,
+		ID:  teamID}
+
+	if err != nil {
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+			if mysqlErr.Number == gfsql.ERR_CODE_DUPLICATE_ENTRY {
+				return nil, ErrDuplicate
+			}
+		}
+		return nil, err
+	}
+
+	if c, err := result.LastInsertId(); err == nil {
+		team.IID = uint32(c)
+	}
+
 	return team, err
 }
