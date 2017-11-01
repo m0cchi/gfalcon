cefee41bb9f2c2291ab3a6b3672a202ee86a31a2
m0cchi <boom.boom.planet@gmail.com>
2017-11-02 08:25:38 +0900

    add Get Action API and Test

--- model/action_test.go
+++ model/action_test.go
@@ -1,35 +1,9 @@
 package model_test
 
 import (
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
 	"github.com/m0cchi/gfalcon/model"
-	"os"
 	"testing"
 )
-
-type Helper struct {
-	DB *sqlx.DB
-}
-
-var helper Helper
-
-func TestMain(m *testing.M) {
-	datasource := os.Getenv("DATASOURCE")
-	if datasource == "" {
-		os.Exit(1)
-	}
-
-	db, err := sqlx.Connect("mysql", datasource)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	defer db.Close()
-	helper = Helper{db}
-	code := m.Run()
-	os.Exit(code)
-}
 
 func TestCreateAction(t *testing.T) {
 	var serviceIID uint32 = 1
@@ -51,4 +25,45 @@ func TestCreateAction(t *testing.T) {
 	if action.ServiceIID < 1 {
 		t.Fatalf("expected IID > 0 but %v", action.ServiceIID)
 	}
+}
+
+func TestGetAction(t *testing.T) {
+	var serviceIID uint32 = 1
+	actionID := "keyaki"
+
+	action, err := model.GetAction(helper.DB, serviceIID, actionID)
+
+	if err != nil {
+		t.Fatalf("has err: %v", err)
+	}
+
+	if action.ID != actionID {
+		t.Fatalf("expected %v but %v", actionID, action.ID)
+	}
+
+	if action.IID < 1 {
+		t.Fatalf("expected IID > 0 but %v", action.IID)
+	}
+
+	if action.ServiceIID != serviceIID {
+		t.Fatalf("expected %v but %v", serviceIID, action.ServiceIID)
+	}
+}
+
+func TestDeleteAction(t *testing.T) {
+	var serviceIID uint32 = 1
+	actionID := "keyaki"
+
+	action, err := model.GetAction(helper.DB, serviceIID, actionID)
+	err = action.Delete(helper.DB)
+
+	if err != nil {
+		t.Fatalf("has err: %v", err)
+	}
+	_, err = model.GetAction(helper.DB, serviceIID, actionID)
+
+	if err == nil {
+		t.Fatalf("missing err: %v", err)
+	}
+
 }
--- model/team_test.go
+++ model/team_test.go
@@ -1,35 +1,9 @@
 package model_test
 
 import (
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
 	"github.com/m0cchi/gfalcon/model"
-	"os"
 	"testing"
 )
-
-type Helper struct {
-	DB *sqlx.DB
-}
-
-var helper Helper
-
-func TestMain(m *testing.M) {
-	datasource := os.Getenv("DATASOURCE")
-	if datasource == "" {
-		os.Exit(1)
-	}
-
-	db, err := sqlx.Connect("mysql", datasource)
-	if err != nil {
-		os.Exit(1)
-	}
-
-	defer db.Close()
-	helper = Helper{db}
-	code := m.Run()
-	os.Exit(code)
-}
 
 func TestCreateTeam(t *testing.T) {
 	teamID := "keyaki"
