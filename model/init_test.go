142566cb19092a269478da16133a9565b6759df0
m0cchi <boom.boom.planet@gmail.com>
2017-11-05 14:18:12 +0900

    add test util

--- model/init_test.go
+++ model/init_test.go
@@ -3,6 +3,7 @@ package model_test
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/m0cchi/gfalcon/model"
 	"os"
 	"testing"
 )
@@ -28,4 +29,12 @@ func TestMain(m *testing.M) {
 	helper = Helper{db}
 	code := m.Run()
 	os.Exit(code)
+}
+
+func GetUser(db *sqlx.DB, teamID string, userID string) (*model.User, error) {
+	team, err := model.GetTeam(db, teamID)
+	if err != nil {
+		return nil, err
+	}
+	return model.GetUser(db, team.IID, userID)
 }
