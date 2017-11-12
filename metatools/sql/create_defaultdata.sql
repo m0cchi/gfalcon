7e973805c986f25ded716502e4d98d2db3535766
m0cchi <boom.boom.planet@gmail.com>
2017-11-12 19:49:45 +0900

    add testdata

--- metatools/sql/create_defaultdata.sql
+++ metatools/sql/create_defaultdata.sql
@@ -4,3 +4,5 @@ INSERT INTO `teams`(`id`) VALUE ('gfalcon');
 INSERT INTO `users`(`team_iid`,`id`) SELECT `iid` as `team_iid`, 'gfadmin' as `id` FROM `teams` WHERE `teams`.`id` = 'gfalcon';
 
 INSERT INTO `action_links`(`action_iid`, `user_iid`) select `actions`.`iid` as `action_iid` ,`users`.`iid` as `user_iid` from (select `iid` from `users`) as `users`, (select `iid` from `actions`) as `actions` where `users`.`iid` = 1 and `actions`.`iid` = 1;
+
+INSERT INTO `roles`(`team_iid`,`id`) SELECT `iid` as `team_iid`, 'gfalcon' as `id` FROM `teams` WHERE `teams`.`id` = 'gfalcon';
